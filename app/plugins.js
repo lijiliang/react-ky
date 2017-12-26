9b80c4f2ebacd156e5c73977f9fee7c60834cd8b
Benson <benson@Benson.local>
2017-12-26 11:32:53 +0800

    feat: js延迟执行

--- app/plugins.js
+++ app/plugins.js
@@ -1,17 +1,19 @@
 import Swiper from 'swiper/dist/js/swiper.min.js';
 
 window.onload = function(){
-    // 公司简介
-    const mySwiper = new Swiper('.swiper-container', {
-        // loop: true,
-        autoHeight: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        // 分页器
-        pagination: {
-            el: '.swiper-pagination',
-        },
-    });
+    setTimeout(() => {
+        // 公司简介
+        const mySwiper = new Swiper('.swiper-container', {
+            // loop: true,
+            autoHeight: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            // 分页器
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });
+    }, 2000);
 };
