c3bef6a1a5a3cd01c6be033734aff5d2f10f47eb
Benson <benson@Benson.local>
2017-12-26 14:14:29 +0800

    feat: 放大地图

--- app/plugins.js
+++ app/plugins.js
@@ -1,7 +1,32 @@
 import Swiper from 'swiper/dist/js/swiper.min.js';
+import PhotoSwipe from 'photoswipe/dist/photoswipe.min.js'
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default.min.js'
 
 window.onload = function(){
     setTimeout(() => {
+        let openPhotoSwipe = function() {
+            let pswpElement = document.querySelectorAll('.pswp')[0];
+            // build items array
+            const items = [
+                {
+                    src: 'https://kyaniyoupaiyun.b0.upaiyun.com/mapcn2017.jpg',
+                    w: 1500,
+                    h: 825
+                }
+            ];
+            // define options (if needed)
+            const options = {
+                history: false,
+                focus: false,
+                showAnimationDuration: 0,
+                hideAnimationDuration: 0
+            };
+            const gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+            gallery.init();
+        };
+        openPhotoSwipe();
+        document.getElementById('photoswipe-about').onclick = openPhotoSwipe;
+
         // 公司简介
         const mySwiper = new Swiper('.swiper-container', {
             // loop: true,
