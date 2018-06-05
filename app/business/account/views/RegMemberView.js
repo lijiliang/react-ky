b9ea8380421e9e99a1e1762fe16ab1902257481a
benson <sucaiweb@163.com>
2018-06-05 11:20:41 +0800

    fix: 解决注册页input foucs

--- app/business/account/views/RegMemberView.js
+++ app/business/account/views/RegMemberView.js
@@ -70,6 +70,15 @@ const cityAreaData = Cache.getObj(Cache.keys.ky_cache_cityArea) || [];
          this.setState({
              recommenderTimes: 0
          })
+        var bfscrolltop = document.body.scrollTop; //获取软键盘唤起前浏览器滚动部分的高度
+        $("input").focus(function(){ // 当它获取焦点时触发事件
+            interval = setInterval(function(){ //设置一个计时器，时间设置与软键盘弹出所需时间相近
+            document.body.scrollTop = document.body.scrollHeight; //获取焦点后将浏览器内所有内容高度赋给浏览器滚动部分高度
+            },100)
+        }).blur(function(){//设定输入框失去焦点时的事件
+            clearInterval(interval);//清除计时器
+            document.body.scrollTop = bfscrolltop; //将软键盘唤起前的浏览器滚动部分高度重新赋给改变后的高度
+        });
      }
 
      // 返回上一页
