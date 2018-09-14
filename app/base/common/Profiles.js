05b6d365bba5d826845555d9c6d7cf086ffeeed8
Benson <sucaiweb@163.com>
2018-09-14 12:32:55 +0800

    feat: api

--- app/base/common/Profiles.js
+++ app/base/common/Profiles.js
@@ -14,7 +14,7 @@ const profiles = {
         // host: '10.206.41.102:8012',
         // host: 'cjmgxp.natappfree.cc',
         // host: 'dev.kyani.cn:8100',
-        host: 'dev.kyani.cn:8100',
+        host: '54.223.215.188',
         //sercureEnable: true
         sercureEnable: false
     }
