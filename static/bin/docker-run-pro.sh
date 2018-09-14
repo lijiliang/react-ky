81a78da56ac416382409ff0565b7bc50c0ebe869
Benson <sucaiweb@163.com>
2018-09-14 11:47:08 +0800

    add: bin

---
+++ static/bin/docker-run-pro.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo docker stop stars_power_shop_mobile_clinet
+sudo docker rm stars_power_shop_mobile_clinet
+sudo docker run --privileged=true  -t -i -d -v /data:/data -p 80:80  --name=stars_power_shop_mobile_clinet  557111830783.dkr.ecr.cn-north-1.amazonaws.com.cn/os_nginx /bin/bash -c "/data/www/stars/stars_power_shop_mobile_clinet/bin/run-pro.sh"
---
+++ static/bin/run-pro.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#覆盖Nginx
+cp -rf /data/www/stars/stars_power_shop_mobile_clinet/bin/nginx.conf /usr/local/nginx/conf/
+/usr/local/nginx/sbin/nginx &
+/bin/bash
